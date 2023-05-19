8c400ccf9152c8cc163e59549d56a652bb0fd8d2
ljf <ljf@ljf.com>
2023-05-19 17:25:13 +0800

    lucene 查询索引

--- dandelion-search/dandelion-search-lucene/src/main/java/org/dandelion/search/lucene/example/LuceneIndex.java
+++ dandelion-search/dandelion-search-lucene/src/main/java/org/dandelion/search/lucene/example/LuceneIndex.java
@@ -2,8 +2,15 @@ package org.dandelion.search.lucene.example;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.lucene.document.*;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.wltea.analyzer.lucene.IKAnalyzer;
@@ -18,8 +25,9 @@ import java.nio.file.Paths;
  */
 public class LuceneIndex {
 
-    public static void main(String[] args) throws IOException {
-        create();
+    public static void main(String[] args) throws Exception {
+//        create();
+        search();
     }
 
     /**
@@ -42,27 +50,54 @@ public class LuceneIndex {
         // 创建 IndexWriter
         IndexWriter indexWriter = new IndexWriter(directory, writerConfig);
         File[] files = path.listFiles();
-        for (File file : files) {
-            // 创建 Document
-            Document document = new Document();
-            // 文件名称
-            document.add(new TextField("fileName", file.getName(), Field.Store.YES));
-            // 文件内容
-            document.add(new TextField("content", FileUtils.readFileToString(file, "UTF-8"), Field.Store.YES));
-            // 文件路径
-            document.add(new StoredField("path", file.getPath()));
-            // 文件大小
-            document.add(new NumericDocValuesField("size", FileUtils.sizeOf(file)));
-            // Document 写入索引
-            indexWriter.addDocument(document);
+        if (files != null) {
+            for (File file : files) {
+                // 创建 Document
+                Document document = new Document();
+                // 文件名称
+                document.add(new TextField("fileName", file.getName(), Field.Store.YES));
+                // 文件内容
+                document.add(new TextField("content", FileUtils.readFileToString(file, "GBK"), Field.Store.YES));
+                // 文件路径
+                document.add(new StoredField("path", file.getPath()));
+                // 文件大小
+                document.add(new NumericDocValuesField("size", FileUtils.sizeOf(file)));
+                // Document 写入索引
+                indexWriter.addDocument(document);
+            }
         }
-        if (indexWriter != null) {
-            indexWriter.close();
-            indexWriter = null;
-        }
+        indexWriter.close();
     }
 
-    public static void search(){
+    /**
+     * 检索查询
+     *
+     * @author liujunfei
+     * @date 2023/5/18
+     **/
+    public static void search() throws IOException, ParseException {
+        // 索引文件存放目录
+        Directory directory = FSDirectory.open(Paths.get("D:\\lucene\\index"));
+        // 创建 IndexReader
+        IndexReader indexReader = DirectoryReader.open(directory);
+        // 创建 IndexSearcher
+        IndexSearcher indexSearcher = new IndexSearcher(indexReader);
+        // 创建 ik 分词器
+        IKAnalyzer ikAnalyzer = new IKAnalyzer();
+        QueryParser parser = new QueryParser("content", ikAnalyzer);
+        // 创建查询
+        Query query = parser.parse("大师");
+        // 获取搜索结果
+        ScoreDoc[] scoreDoc = indexSearcher.search(query, 10).scoreDocs;
+        for (ScoreDoc doc : scoreDoc) {
+            Document document = indexSearcher.doc(doc.doc);
+            String fileName = document.get("fileName");
+
+            System.out.println(fileName);
+        }
+
+        indexReader.close();
+        directory.close();
 
     }
 }
