c0d15c3c9cca9d7361fc4187dfc23402c2225185
l <l>
2023-08-08 10:19:18 +0800

    easyexecl 工具使用

---
+++ dandelion-easyexecl/src/main/java/org/dandelion/easyexecl/DandelionEasyexeclApplication.java
@@ -0,0 +1,13 @@
+package org.dandelion.easyexecl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DandelionEasyexeclApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DandelionEasyexeclApplication.class, args);
+	}
+
+}
---
+++ dandelion-easyexecl/src/test/java/org/dandelion/easyexecl/DandelionEasyexeclApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dandelion.easyexecl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DandelionEasyexeclApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
