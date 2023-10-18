c1509497a1fb8a15e5502a6903c1cb103e2b65f8
l <l>
2023-10-18 17:58:16 +0800

    liquibase

---
+++ dandelion-liquibase/src/main/java/org/dandelion/liquibase/LiquibaseApplication.java
@@ -0,0 +1,16 @@
+package org.dandelion.liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lx6x
+ * @date 2023/10/18
+ */
+@SpringBootApplication
+public class LiquibaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseApplication.class,args);
+    }
+}
