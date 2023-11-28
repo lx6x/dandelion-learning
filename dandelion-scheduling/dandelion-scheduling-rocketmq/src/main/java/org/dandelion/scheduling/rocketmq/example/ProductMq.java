a33511bb9b375019faec2374e0f5c34755f63bde
l <l>
2023-11-28 16:27:21 +0800

    .

--- dandelion-scheduling/dandelion-scheduling-rocketmq/src/main/java/org/dandelion/scheduling/rocketmq/example/ProductMq.java
+++ dandelion-scheduling/dandelion-scheduling-rocketmq/src/main/java/org/dandelion/scheduling/rocketmq/example/ProductMq.java
@@ -3,10 +3,6 @@ package org.dandelion.scheduling.rocketmq.example;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
-import org.apache.rocketmq.spring.core.RocketMQTemplate;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class ProductMq {
     public static void main(String[] args) throws Exception {
