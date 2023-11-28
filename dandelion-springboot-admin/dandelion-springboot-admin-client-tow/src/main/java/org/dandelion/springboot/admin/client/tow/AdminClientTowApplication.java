0dcc80678391ced43079468970a3c79ebe009abb
lx6x <xiayong12321@163.com>
2023-11-28 21:15:57 +0800

    springbootadmin 监控示例

---
+++ dandelion-springboot-admin/dandelion-springboot-admin-client-tow/src/main/java/org/dandelion/springboot/admin/client/tow/AdminClientTowApplication.java
@@ -0,0 +1,15 @@
+package org.dandelion.springboot.admin.client.tow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lx6x
+ * @date 2023/11/28
+ */
+@SpringBootApplication
+public class AdminClientTowApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminClientTowApplication.class, args);
+    }
+}
