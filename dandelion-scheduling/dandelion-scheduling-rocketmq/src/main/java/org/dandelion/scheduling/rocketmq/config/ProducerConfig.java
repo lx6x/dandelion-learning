11613004a33b77408bf1e208e0a20dcbebc3552d
lx6x <l>
2023-12-19 16:26:32 +0800

    .

---
+++ dandelion-scheduling/dandelion-scheduling-rocketmq/src/main/java/org/dandelion/scheduling/rocketmq/config/ProducerConfig.java
@@ -0,0 +1,21 @@
+package org.dandelion.scheduling.rocketmq.config;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author lx6x
+ * @date 2023/5/12
+ */
+/*@Configuration
+public class ProducerConfig {
+
+    @Bean
+    public DefaultMQProducer producer(){
+        DefaultMQProducer producer = new DefaultMQProducer("producer-group-1");
+        producer.setNamesrvAddr("your.namesrv.addr:9876");
+        // 其他配置...
+        return producer;
+    }
+}*/
